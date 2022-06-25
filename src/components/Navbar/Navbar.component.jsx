0cc5f030d2da1ca90051af9dba57dacf647599d6
Julian Ifesiokwu <julianifesiokwu@gmail.com>
2022-06-25 12:57:54 +0100

    working on mobile responsiveness of navbar

--- src/components/Navbar/Navbar.component.jsx
+++ src/components/Navbar/Navbar.component.jsx
@@ -1,15 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { NavLink } from 'react-router-dom'
+import { MdMenu, MdClose } from "react-icons/md";
+import './Navbar.styles.css'
+
 
 const Navbar = () => {
+  const [menu, setShowMenu] = useState(false)
+
+  const showMenu = () => {
+    setShowMenu(!menu)
+  }
+
   return (
     <div className="navbar">
         <p className="logo">JulianIfesiokwu</p>
-        <ul className="navlist">
-            <NavLink to="/" className="nav-item">01.About</NavLink>
-            <NavLink to="/" className="nav-item">02.Projects</NavLink>
-            <NavLink to="/" className="nav-item">03.Contact</NavLink>
-        </ul>
+
+        <div className="navigation">
+          {menu ?  <MdClose onClick={showMenu}  className="icon" size="40" /> : <MdMenu  onClick={showMenu} className="icon" size="40"/> }
+          <ul className={menu ? "navlist active" : "navlist"}>
+              <NavLink to="/" className="nav-item"><span className="index">01.</span> About</NavLink>
+              <NavLink to="/" className="nav-item"><span className="index">02.</span> Projects</NavLink>
+              <NavLink to="/" className="nav-item"><span className="index">03.</span> Contact</NavLink>
+          </ul>
+        </div>
     </div>
   )
 }
