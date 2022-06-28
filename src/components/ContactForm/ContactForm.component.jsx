18bb9fbc7325af11ce1577c16065f05ac46c8122
Julian Ifesiokwu <julianifesiokwu@gmail.com>
2022-06-28 13:55:16 +0100

    created contact form, finished mobile and web views and moved contact form to seperate component

---
+++ src/components/ContactForm/ContactForm.component.jsx
@@ -0,0 +1,48 @@
+import { Link } from "react-router-dom";
+import Button from "../Button/Button.component";
+import { FaLinkedin,FaGithub } from "react-icons/fa";
+import { SiFrontendmentor } from "react-icons/si";
+import { useForm } from 'react-hook-form'
+import "./ContactForm.styles.scss"
+
+const ContactForm = () => {
+    const { register, handleSubmit, formState: { errors } } = useForm()
+
+    const onSubmit = (data) => {
+        console.log(data)
+    }
+
+    return (
+            <section className="contact-form">
+                <article className="contact-info">
+                    <h2 className='contact-title'>Let's get in touch.</h2>
+                    <p>Either by email or through social media.</p>
+                    <div className="social">
+                        <Link to='/'><FaGithub size='30' /></Link>
+                        <Link to='/'><FaLinkedin size='30' /></Link>
+                        <Link to='/'><SiFrontendmentor size='30' /></Link>
+                    </div>
+                </article>
+                <form className="message-form" onSubmit={handleSubmit(onSubmit)}>
+                    <div className="details">
+                        <div className="details-input">
+                            <label htmlFor="" className="name-label">Name</label>
+                            <input type="text" className="client-name" placeholder="Name" {...register('clientName', {required: "Enter your name"})} />
+                            {errors.clientName && <span className="error-message">{errors.clientName.message}</span>}
+                        </div>
+                        <div className="details-input">
+                            <label htmlFor="" className="email-label">Email</label>
+                            <input type="email" className="client-email" placeholder="E-mail" {...register("clientEmail", {required: "Enter your email"})} />
+                            {errors.clientEmail && <span className="error-message">{errors.clientEmail.message}</span>}
+                        </div>
+                    </div>
+                    <div className="message-container">
+                        <textarea name="" id="" className='message' placeholder="Your message here" {...register("clientMessage", {required: "Enter your message"})}></textarea>
+                        {errors.clientMessage && <span className="error-message">{errors.clientMessage.message}</span>}
+                    </div>
+                    <Button title={`Contact me!`}/>
+                </form>
+            </section>
+    )}
+
+    export default ContactForm
--- src/components/Homepage/Homepage.component.jsx
+++ src/components/Homepage/Homepage.component.jsx
@@ -1,13 +1,9 @@
-import { Link } from 'react-router-dom';
-import { useForm } from 'react-hook-form'
+import ContactForm from '../ContactForm/ContactForm.component'
 import Button from '../Button/Button.component'
-import { FaLinkedin,FaGithub } from "react-icons/fa";
 
 import './Homepage.styles.scss'
 
-
 const Homepage = () => {
-    const { register, handleSubmit, formState: { errors } } = useForm()
 
     return (
         <div className="homepage">
@@ -35,34 +31,8 @@ const Homepage = () => {
                 </article>
                 <article className="fav-projects"></article>
             </section>
-            <section className="contact-form">
-                <article className="contact-info">
-                    <h2 className='contact-title'>Let's get in touch.</h2>
-                    <p>Either by email or through social media.</p>
-                    <div className="social">
-                        <Link to='/'><FaGithub size='30' /></Link>
-                        <Link to='/'><FaLinkedin size='30' /></Link>
-                    </div>
-                </article>
-                <form className="message-form">
-                    <div className="details">
-                        <div className="details-input">
-                            <label htmlFor="" className="name-label">Name
-                                <input type="text" className="client-name" placeholder="Name" />
-                            </label>
-                        </div>
-                        <div className="details-input">
-                            <label htmlFor="" className="email-label">Email
-                                <input type="email" className="client-email" placeholder="E-mail" />
-                            </label>
-                        </div>
-                    </div>
-                    <div className="message-container">
-                        <textarea name="" id="" className='message'></textarea>
-                    </div>
-                    <Button title={`Message me!`}/>
-                </form>
-            </section>
+            <ContactForm />
+
         </div>
     )
 }
