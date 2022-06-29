753792b141ced93d89af3c4d96bb166b53814c61
Julian Ifesiokwu <julianifesiokwu@gmail.com>
2022-06-29 14:09:15 +0100

    added and styled footer of project

---
+++ src/components/Footer/Footer.component.jsx
@@ -0,0 +1,11 @@
+import "./Footer.styles.scss";
+
+const Footer = () => {
+    return (
+        <footer className="footer">
+            <p>This is the footer</p>
+        </footer>
+    )
+}
+
+export default Footer
