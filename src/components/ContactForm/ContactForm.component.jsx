9b3589d3ca9be799a08bdf6fbedd4e0b1d0bac3b
Julian Ifesiokwu <julianifesiokwu@gmail.com>
2023-01-16 12:34:50 +0100

    refactored contact form, removed react-hook-form from project

--- src/components/ContactForm/ContactForm.component.jsx
+++ src/components/ContactForm/ContactForm.component.jsx
@@ -1,27 +1,17 @@
-import { FaLinkedin, FaGithub } from "react-icons/fa";
+import React, { useContext } from 'react';
+import { FaLinkedin, FaGithub, FaTwitter } from "react-icons/fa";
 import { SiFrontendmentor } from "react-icons/si";
-import { useForm } from "react-hook-form";
-import "./ContactForm.styles.scss";
+import { contactFormContext } from '../../contexts/contactFormContext';
+import './ContactForm.styles.css';
 
 const ContactForm = () => {
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm();
-
-  const onSubmit = (data) => {
-    console.log(data);
-  };
-
+  const { handleChange, handleSubmit } = useContext(contactFormContext);
   return (
-    <section className="contact-form">
+    <section className="contact-form" id='contact'>
       <article className="contact-info">
-        <h2 className="contact-title">Let's get in touch.</h2>
-        <p className="contact-brief">
-          Either by email or through social media.
-        </p>
-        <div className="social">
+        <h2 className="big-title">Let's Connect.</h2>
+        <p>Find me on any of the platforms below or send me a mail.</p>
+        <div className="social-form-links">
           <a
             href="https://github.com/JulianIfesiokwu"
             target="_blank"
@@ -36,7 +26,15 @@ const ContactForm = () => {
             rel="noreferrer"
             className="social-link"
           >
-            <FaLinkedin size="40" className="social-links" />
+            <FaLinkedin size="40" className="social-links" color='#0072b1' />
+          </a>
+          <a
+            href="https://twitter.com/mrjules07"
+            target="_blank"
+            rel="noreferrer"
+            className="social-link"
+          >
+            <FaTwitter size="40" className="social-links" color='#00acee' />
           </a>
           <a
             href="https://www.frontendmentor.io/dashboard/my-challenges?tab=completed"
@@ -52,66 +50,40 @@ const ContactForm = () => {
         className="message-form"
         action="https://formsubmit.co/julianifesiokwu@gmail.com"
         method="POST"
+        onSubmit={handleSubmit}
       >
         <div className="details">
-          <div className="details-input">
-            <label htmlFor="" className="name-label">
-              Name
-            </label>
             <input
               name="Client-name"
               type="text"
               className="client-name"
               placeholder="Name"
-              {...register("clientName", { required: "Oops! Enter your name" })}
+              onChange={handleChange}
             />
-            {errors.clientName && (
-              <span className="error-message">{errors.clientName.message}</span>
-            )}
-          </div>
-          <div className="details-input">
-            <label htmlFor="" className="email-label">
-              Email
-            </label>
             <input
               name="Client-email"
               type="email"
               className="client-email"
               placeholder="E-mail"
-              {...register("clientEmail", {
-                required: "Oops! Your email is required.",
-              })}
+              onChange={handleChange}
             />
-            {errors.clientEmail && (
-              <span className="error-message">
-                {errors.clientEmail.message}
-              </span>
-            )}
-          </div>
         </div>
         <div className="message-container">
-          <label htmlFor="" className="message-label">
-            Your message
-          </label>
           <textarea
+            rows='8'
             name="Client-Message"
             id="message-label"
             className="message"
             placeholder="Your message here..."
-            {...register("clientMessage", { required: "Your message" })}
-          ></textarea>
-          {errors.clientMessage && (
-            <span className="error-message">
-              {errors.clientMessage.message}
-            </span>
-          )}
+            onChange={handleChange}
+          />
         </div>
         <button type="submit" className="contact-btn">
-          Contact me!
+          Send Mail
         </button>
       </form>
     </section>
-  );
-};
+  )
+}
 
 export default ContactForm;
