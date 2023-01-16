3fccd0472f0ae1df82cb2953166d1a8864cd8591
Julian Ifesiokwu <julianifesiokwu@gmail.com>
2023-01-16 12:44:00 +0100

    refactored copyright to be dynamic using js and styled mobile and web views

--- src/components/Footer/Footer.component.jsx
+++ src/components/Footer/Footer.component.jsx
@@ -1,14 +1,54 @@
-import "./Footer.styles.scss";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { FaLinkedin, FaGithub, FaTwitter } from "react-icons/fa";
+import { SiFrontendmentor } from "react-icons/si";
+import './Footer.styles.css';
 
 const Footer = () => {
-  const today = new Date();
-  const year = today.getFullYear();
+    const d = new Date();
+    let year = d.getFullYear();
 
-  return (
-    <footer className="footer">
-      <p>&copy; {year}</p>
-    </footer>
-  );
+    return (
+        <div className='footer'>
+            <Link to='/'>developerjulian</Link>
+            <p>Copyright {year}</p>
+            <div className="social">
+                <a
+                    href="https://github.com/JulianIfesiokwu"
+                    target="_blank"
+                    rel="noreferrer"
+                    className="social-link"
+                >
+                    <FaGithub size="25" className="social-links"/>
+                </a>
+                <a
+                    href="https://www.linkedin.com/in/julian-ifesiokwu-1307/"
+                    target="_blank"
+                    rel="noreferrer"
+                    className="social-link"
+                >
+                    <FaLinkedin size="25" className="social-links" color='#0072b1' />
+                </a>
+                <a
+                    href="https://twitter.com/mrjules07"
+                    target="_blank"
+                    rel="noreferrer"
+                    className="social-link"
+                >
+                <FaTwitter size="25" className="social-links" color='#00acee' />
+                </a>
+                <a
+                    href="https://www.frontendmentor.io/dashboard/my-challenges?tab=completed"
+                    target="_blank"
+                    rel="noreferrer"
+                    className="social-link"
+                >
+                    <SiFrontendmentor size="25" className="social-links" />
+                </a>
+                </div>
+            
+        </div>
+    )
 };
 
 export default Footer;
