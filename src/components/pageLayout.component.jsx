e5df5b6cf6a6987b126ba156a5aefa2c5574f753
Julian Ifesiokwu <julianifesiokwu@gmail.com>
2023-01-18 18:19:17 +0100

    adding footer component to pageLayout component

--- src/components/pageLayout.component.jsx
+++ src/components/pageLayout.component.jsx
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react';
 import { Outlet } from 'react-router-dom';
 import Header from "./header/Header.component";
+import FooterComponent from "./footer/Footer.component";
 
 import { themeContext } from "../contexts/themeContext";
 
@@ -11,6 +12,7 @@ function PageLayout() {
         <div className={theme ? 'light' : 'dark'}>
             <Header />
             <Outlet />
+            <FooterComponent />
         </div>
     )
 };
